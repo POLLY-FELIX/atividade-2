b80df6808e0b08cd664bcf5244710dc1bb27fe83
Aluno <Aluno@DESKTOP-SANL2T8>
2024-01-18 21:37:00 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividade_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random numAleatorio = new Random();
+            int valorInteiro = numAleatorio.Next(10,50);
+
+            decimal primeiro_valor;
+            decimal segundo_valor;
+            decimal terceiro_valor;
+
+            Console.WriteLine("voce tera tres tentativa para acertar o numero sorteado");
+            primeiro_valor = decimal.Parse(Console.ReadLine());
+
+            if (primeiro_valor == valorInteiro)
+            { Console.WriteLine("o valor esta correto");
+            Console.WriteLine("o valor é  =");
+
+             Console.WriteLine("voce possui 3 tentativas para acerta o numero');
+
+        }
+    }
+}
